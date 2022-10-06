9d60d4299a353c054168c6096e89c218f79b7334
TerrySu <bjsuzhe@163.com>
2022-10-06 12:19:07 +0800

    update webpack.gp

--- bin/webpack.gp.js
+++ bin/webpack.gp.js
@@ -10,7 +10,7 @@ module.exports = merge(base, {
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, '../docs'),
-    publicPath: '/docs'
+    publicPath: './'
   },
   plugins: [
     new MiniCssExtractPlugin({
