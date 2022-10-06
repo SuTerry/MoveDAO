f9a2bd9d3d1bb68cc60fd1a8e1aeb8c5ae524b8b
TerrySu <bjsuzhe@163.com>
2022-10-06 12:41:50 +0800

    update docs

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import 'core-js/es/set'
 
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from '@store/index'
 import App from './App'
@@ -21,9 +22,9 @@ const root = createRoot((document.getElementById('root') as Element))
 window.addEventListener("load", () => {
   root.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   )
 })
